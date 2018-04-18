ff43485076c1081e4f27d05c5a84ad309cfdd644
olezt <lfants666@gmail.com>
2018-04-18 11:52:36 +0300

    Update Download functionality to download selected date's apod

--- src/main/java/com/nasaApod/Constants.java
+++ src/main/java/com/nasaApod/Constants.java
@@ -8,5 +8,9 @@ public final class Constants {
 	public static final String APOD_API_KEY_PARAM_NAME = "api_key";
 	public static final String APOD_API_KEY_PARAM_VALUE = "DEMO_KEY";
 	public static final String APOD_API_DATE_PARAM_NAME = "date";
-			
+	
+	public static final String APOD_DOWNLOAD_CONTROLLER_PDF_CELL_TITLE = "Title:";
+	public static final String APOD_DOWNLOAD_CONTROLLER_PDF_CELL_COPYRIGHT = "Copyright:";
+	public static final String APOD_DOWNLOAD_CONTROLLER_PDF_CELL_EXPLANATION = "Explanation:";
+
 }
--- src/main/java/com/nasaApod/controllers/DownloadController.java
+++ src/main/java/com/nasaApod/controllers/DownloadController.java
@@ -2,21 +2,27 @@ package com.nasaApod.controllers;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.util.StringUtils;
+
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
+import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
-//import com.nasaApod.enums.QuoteEnum;
+import com.nasaApod.Constants;
+import com.nasaApod.entities.Apod;
 
 import lombok.Cleanup;
 
@@ -26,37 +32,47 @@ public class DownloadController {
 	public DownloadController() {}
 
 	/**
-	 * Create the pdf document containing all quotes
+	 * Create the pdf document containing the selected date's apod
 	 * @param document
+	 * @param apod
 	 * @throws DocumentException
+	 * @throws MalformedURLException
+	 * @throws IOException
 	 */
-	public void createAllQuotesPDF(Document document) throws DocumentException {
-        Font headerFont = FontFactory.getFont(FontFactory.COURIER_BOLD, 25);
-        Font cellFont = FontFactory.getFont(FontFactory.COURIER, 15);
+	public void createAllQuotesPDF(Document document, Apod apod) throws DocumentException, MalformedURLException, IOException {
+        Font headerFont = FontFactory.getFont(FontFactory.COURIER_BOLD, 15);
+        Font cellFont = FontFactory.getFont(FontFactory.COURIER, 7);
         
-		PdfPTable quotesTable = new PdfPTable(new float[] { 15, 60, 25 });
-		quotesTable.setWidthPercentage(90);
-		quotesTable.setHeaderRows(1);
+		PdfPTable apodTable = new PdfPTable(new float[] { 30, 70 });
+		apodTable.setWidthPercentage(90);
 
-        quotesTable.addCell(new PdfPCell(new Phrase("Who", headerFont)));
-        quotesTable.addCell(new PdfPCell(new Phrase("What", headerFont)));
-        quotesTable.addCell(new PdfPCell(new Phrase("When", headerFont)));
+		//image cell
+		Image img = Image.getInstance(new URL(apod.getUrl()));
+		PdfPCell imgCell = new PdfPCell(img, true);
+		imgCell.setColspan(2);
+		apodTable.addCell(imgCell);
+		//title cells
+		apodTable.addCell(new PdfPCell(new Phrase(Constants.APOD_DOWNLOAD_CONTROLLER_PDF_CELL_TITLE, headerFont)));
+		apodTable.addCell(new PdfPCell(new Phrase(apod.getTitle(), cellFont)));
+		//copyright cells
+		if(!StringUtils.isEmpty(apod.getCopyright())){
+			apodTable.addCell(new PdfPCell(new Phrase(Constants.APOD_DOWNLOAD_CONTROLLER_PDF_CELL_COPYRIGHT, headerFont)));
+			apodTable.addCell(new PdfPCell(new Phrase(apod.getCopyright(), cellFont)));
+		}
+		//explanation cells
+		apodTable.addCell(new PdfPCell(new Phrase(Constants.APOD_DOWNLOAD_CONTROLLER_PDF_CELL_EXPLANATION, headerFont)));
+		apodTable.addCell(new PdfPCell(new Phrase(apod.getExplanation(), cellFont)));
 
-//        for(QuoteEnum qe : QuoteEnum.values()) {
-//	        quotesTable.addCell(new PdfPCell(new Phrase(qe.getUser(), cellFont)));
-//	        quotesTable.addCell(new PdfPCell(new Phrase(qe.getText(), cellFont)));
-//	        quotesTable.addCell(new PdfPCell(new Phrase(qe.getDate(), cellFont)));
-//        }
-		
-        document.add(quotesTable);
+        document.add(apodTable);
 	}
 	
 	/**
-	 * Download a pdf which contains all quotes
+	 * Download a pdf which contains selected date's apod
+	 * @param apod
 	 * @throws IOException
 	 */
-	public void downloadAllQuotesPDF() throws IOException {
-		String fileName = "allEDQuotes.pdf";
+	public void downloadApodPDF(Apod apod) throws IOException {
+		String fileName = "Apod-"+apod.getDate()+".pdf";
 		String contentType = "application/pdf";
 		
 		FacesContext fc = FacesContext.getCurrentInstance();
@@ -68,12 +84,12 @@ public class DownloadController {
 
         try {
         	@Cleanup OutputStream output = response.getOutputStream();
-    	    @Cleanup Document document = new Document(PageSize.A4.rotate());
+    	    @Cleanup Document document = new Document(PageSize.A4);
             PdfWriter.getInstance(document, output);
             
             document.open();
         	
-			createAllQuotesPDF(document);
+			createAllQuotesPDF(document, apod);
 		} catch (DocumentException | IOException e) {
 			e.printStackTrace();
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
